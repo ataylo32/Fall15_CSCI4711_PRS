b3676b4086d5264271d9e8e8f8e4fafdc63d4ab8
ataylo32 <alextay1234@gmail.com>
2015-10-28 15:22:08 -0400

    fixed boundaries

--- ProductRatingSystem/Controller/ApplicationController.cs
+++ ProductRatingSystem/Controller/ApplicationController.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ProductRatingSystem.Controller
 {
-    class ApplicationController
+    class ApplicationController:Controller
     {
+        public void Logout()
+        {
+            Application.Restart();
+        }
     }
 }
