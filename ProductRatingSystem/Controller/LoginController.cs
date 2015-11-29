3b93c64b657c499589d2cbd96127fed67fa9529c
Dunnothenerd1 <Dunnothenerd@gmail.com>
2015-11-29 02:17:15 -0500

    Did some stuff

--- ProductRatingSystem/Controller/LoginController.cs
+++ ProductRatingSystem/Controller/LoginController.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace ProductRatingSystem.Controller
 {
     class LoginController : Controller
     {
-        public void Login()
+
+        string connection = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=|DataDirectory|\ProductRatingSystem.mdf;Integrated Security=True";
+
+
+
+        public bool Login(string name, string password)
         {
+            using(SqlConnection mydbconn = new SqlConnection(connection))
+            {
 
-
+            }
+            return true;
         }
 
 
